2d719b0fc57ad65f1186c3024e6fdd7dde50c05b
Avery Schreiner <schreineravery@gmail.com>
2024-05-25 14:51:16 -0500

    add .gitignore

--- obj/Debug/net8.0/Chefster.AssemblyInfo.cs
+++ obj/Debug/net8.0/Chefster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chefster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7711584fc00734ad9a0724ac35fcdd3d35473590")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chefster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chefster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
